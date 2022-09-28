d53c1ea96b8560c38146b35ab2269d9d12301910
hannes <hannesdelbeke@gmail.com>
2022-09-28 16:01:51 -0700

    util to install utils to menu
    depends on unreal menu (optional dependency)

---
+++ unreal_qt/utils/__init__.py
@@ -0,0 +1,22 @@
+def install_to_menu():
+    import unreal_menu
+    data = {
+        "parent_menu": "LevelEditor.MainMenu.Tools",
+        "items": [
+            {
+                "name": "Qt Utils",
+                "items": [
+                    {
+                        "name": "Icon Browser",
+                        "items": [
+                            {
+                                "name": "Open Icon Browser",
+                                "command": "import unreal_qt.utils.icons"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+    unreal_menu.setup_menu(data)
