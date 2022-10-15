9883df96682efb7557efa600a4f6257c672bfa40
hannes <hannesdelbeke@gmail.com>
2022-10-15 10:40:07 -0700

    add basic widget manager

--- unreal_qt/__init__.py
+++ unreal_qt/__init__.py
@@ -19,3 +19,49 @@ def setup():
     unreal_qt.unrealStylesheet.main.setup()
 
     print("Completed unreal_qt setup")
+
+class widget_manager():
+    widgets = []
+
+    @classmethod
+    def _wrap_closeEvent(cls, widget, original_closeEvent):  # noqa
+        # due to the overriding method on an instance, self is not passed anymore in closeEvent!
+
+        def closeEvent(event=None):  # noqa
+            # run original closeEvent
+            result = original_closeEvent(event)
+
+            # remove widget from list
+            close_widget = event.isAccepted()
+            if close_widget:
+                cls.remove_widget(widget)
+
+            # return original result
+            return result
+
+        setattr(widget, "closeEvent", closeEvent)
+
+        return closeEvent
+
+    @classmethod
+    def add_widget(cls, widget: QtWidgets.QWidget):
+        """
+        Add a widget to the widget manager.
+        - prevent widget from being garbage collected
+        - hookup closeEvent to remove widget from manager
+        - add dark window bar to mimic Unreal's visual style
+        """
+        if widget in cls.widgets:
+            return
+
+        cls.widgets.append(widget)
+        cls._wrap_closeEvent(widget, widget.closeEvent)
+
+
+    @classmethod
+    def remove_widget(cls, widget):
+        cls.widgets.remove(widget)
+
+def wrap(widget):
+    widget_manager.add_widget(widget)
+    return widget
