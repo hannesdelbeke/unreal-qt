843e97a81c8c032b4a7a5581df182dc950b8b584
hannes <hannesdelbeke@gmail.com>
2022-10-15 10:39:44 -0700

    cleanup

--- unreal_qt/unrealStylesheet/main.py
+++ unreal_qt/unrealStylesheet/main.py
@@ -1,15 +1,14 @@
 import sys
 import os
 
-from PySide2 import QtWidgets, QtCore, QtGui, QtUiTools
-# from PySide2 import _loadUi
+from PySide2 import QtWidgets, QtCore
 
 
 MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
-UI_PATH = os.path.join(MODULE_PATH, 'ui', 'progress.ui')
+UI_PATH = os.path.join(MODULE_PATH, 'ui', 'editor.ui')
 QSS_PATH = os.path.join(MODULE_PATH, 'ue.qss')
 
-# if __name__ == '__main__':
+
 def setup():
     app = QtWidgets.QApplication.instance()
     if not app:
@@ -21,8 +20,3 @@ def setup():
     with open(QSS_PATH, 'r') as f:
         qss = f.read()
         app.setStyleSheet(qss)
-
-    window = QtWidgets.QMainWindow()
-    QtUiTools.QUiLoader().load(UI_PATH, window)  # _loadUi(UI_PATH, window)
-    window.show()
-    # sys.exit(app.exec_())
