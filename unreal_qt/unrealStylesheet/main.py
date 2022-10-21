50ae8a8445a1ceab1da579c34c95ff8477d41343
hannes <hannesdelbeke@gmail.com>
2022-10-21 16:00:02 -0700

    fix icons not loading

--- unreal_qt/unrealStylesheet/main.py
+++ unreal_qt/unrealStylesheet/main.py
@@ -2,11 +2,11 @@ import sys
 import os
 
 from PySide2 import QtWidgets, QtCore
+from pathlib import Path
 
 
-MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
-UI_PATH = os.path.join(MODULE_PATH, 'ui', 'editor.ui')
-QSS_PATH = os.path.join(MODULE_PATH, 'ue.qss')
+MODULE_PATH = Path(__file__).parent.resolve()
+QSS_PATH = MODULE_PATH / 'ue.qss'
 
 
 def setup():
@@ -15,7 +15,7 @@ def setup():
         QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
         app = QtWidgets.QApplication(sys.argv)
 
-    QtCore.QResource.registerResource("icons.rcc")
+    QtCore.QResource.registerResource(str(MODULE_PATH / "icons.rcc"))
 
     with open(QSS_PATH, 'r') as f:
         qss = f.read()
